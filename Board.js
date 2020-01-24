8be9d7fe4d68feb785702e6f647251de120e5067
Giovanni Tedesco <giovannitedesco@Johns-MacBook-Air.local>
2020-01-23 20:13:44 -0500

    fixes some minor bugs

--- Board.js
+++ Board.js
@@ -38,9 +38,9 @@ class Board {
 
 		for(let k = 0; k < dirs.length; k++) {
 			new_i = i + dirs[k][0];
-			console.log("New I is: " + new_i);
+			// console.log("New I is: " + new_i);
 			new_j = j + dirs[k][1];
-			console.log("New J is: " + new_j);
+			// console.log("New J is: " + new_j);
 
 			if(new_i >= 0 && new_i < this.board.length && new_j >= 0 && new_j < this.board.length) {
 				if(this.board[new_i][new_j] == -1) {
@@ -58,7 +58,7 @@ class Board {
 		for(let i = 0; i < this.board.length; i++) {
 			for(let j = 0; j < this.board.length; j++) {
 
-				console.log("I: " + i + "J: " + j);
+				// console.log("I: " + i + "J: " + j);
 				this.board[i][j] += this.checkSurrounding(i, j);
 
 			}
@@ -68,7 +68,7 @@ class Board {
 
 	reveal(i, j) {
 		if(this.board[i][j] == -1){
-			console.log("Game over");
+			// console.log("Game over");
 			return true;
 		} else if(this.board[i][j] != 0) {
 			this.revealed[i][j] = true;
@@ -86,7 +86,7 @@ class Board {
 
 	revealHelper(i, j) {
 
-		if(i == 0 || i >= this.board.length || j == 0 || j >= this.board.length || this.revealed[i][j] == true || this.board[i][j] == -1) {
+		if(i < 0 || i >= this.board.length || j < 0 || j >= this.board.length || this.revealed[i][j] == true || this.board[i][j] == -1) {
 			return;
 		} else if(this.board[i][j] > 0) {
 			this.revealed[i][j] = true;
